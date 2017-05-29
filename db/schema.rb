939a277b028774529a82a1e018ea7f32f70471e3
Gabriel Pimont Nogues <gpimontnogues@gmail.com>
2017-05-29 18:13:07 +0200

    working on seed again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529143836) do
+ActiveRecord::Schema.define(version: 20170529152507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170529143836) do
     t.integer  "stock"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
   end
 
   create_table "users", force: :cascade do |t|
