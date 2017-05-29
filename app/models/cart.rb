677352e4815eedc4f5e394e5b813aea926105f99
Fiona Picot <picotfiona@gmail.com>
2017-05-29 17:54:33 +0200

    models and migrations are done

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,5 @@
 class Cart < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  has_many :cart_items
+  validates :cart_items, uniqueness: { scope: :pill }
 end
--- app/models/pill.rb
+++ app/models/pill.rb
@@ -1,2 +1,3 @@
 class Pill < ApplicationRecord
+  has_many :cart_items
 end
