ce0b91421429f0131dcd6c9f2b7b0c154fdc3da1
Fiona Picot <picotfiona@gmail.com>
2017-05-29 17:37:15 +0200

    generated models and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529123736) do
+ActiveRecord::Schema.define(version: 20170529143836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "pill_id"
+    t.integer  "quantity"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id", using: :btree
+    t.index ["pill_id"], name: "index_cart_items_on_pill_id", using: :btree
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id", using: :btree
+  end
+
+  create_table "pills", force: :cascade do |t|
+    t.string   "name"
+    t.string   "brand"
+    t.text     "description"
+    t.integer  "price"
+    t.string   "category"
+    t.integer  "stock"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +56,14 @@ ActiveRecord::Schema.define(version: 20170529123736) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "pills"
+  add_foreign_key "carts", "users"
 end
