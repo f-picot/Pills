4ad72e1125e4f84d661e4d69b513f712592ab07e
clement peneranda <clement@peneranda.fr>
2017-05-31 15:48:32 +0200

    fix index

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :user, optional: true
   has_many :cart_items
-  validates :cart_items, uniqueness: { scope: :pill }
 
   def total
     self.cart_items.inject(0) { |sum, cart_item| sum + (cart_item.pill.price * cart_item.quantity) }
