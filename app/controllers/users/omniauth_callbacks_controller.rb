69e2e68f74dfa2b66812037e14e39f4fc19d20d2
Fiona Picot <picotfiona@gmail.com>
2017-06-07 18:38:01 +0200

    route for stripe is done

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
 
     if user.persisted?
-      sign_in_and_redirect user, event: :authentication
+      sign_in user, event: :authentication
+      redirect_to new_payment_path, notice: 'Signed in successfully.'
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to new_payment_path
     end
   end
 end
