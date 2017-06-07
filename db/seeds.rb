7e42f4366cb8a1bdedb671b1866b0d88e280b289
clement peneranda <clement@peneranda.fr>
2017-06-07 16:38:38 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,20 +14,19 @@ puts "All pills destroyed!"
 doliprane = Pill.create!(
   name: "Doliprane",
   brand: "Sanofi",
-  description: "Paracetamol, médicament pour les maux de tête",
+  description: "Médicament pour soigner les maux de tête",
   price: 2.10,
-  category: "Fièvres",
+  category: "Douleurs",
   stock: 100,
   photo: "doliprane.png",
   brandphoto: "sanofi.png",
   capacity: "8 SACHETS DOSES"
-  price_cents: 210
   )
 
 spasfon = Pill.create!(
   name: "Spasfon",
   brand: "Teva",
-  description: "Produit de référence indiqué contre les spasmes intestinaux",
+  description: "Médicament indiqué contre les spasmes intestinaux",
   price: 2.99,
   category: "Douleurs",
   stock: 50,
@@ -39,9 +38,9 @@ spasfon = Pill.create!(
 aspegic = Pill.create!(
   name: "Aspegic",
   brand: "Sanofi",
-  description: "Médicament pour les maux de tête",
+  description: "Médicament indiqués pour traiter les maux de tête",
   price: 5.50,
-  category: "Fièvres",
+  category: "Douleurs",
   stock: 70,
   photo: "aspegic.png",
   brandphoto: "sanofi.png",
@@ -51,9 +50,9 @@ aspegic = Pill.create!(
 smecta = Pill.create!(
   name: "Smecta",
   brand: "Ipsen",
-  description: "Médicament pour les maux de ventre",
+  description: "Médicament pour traiter la diarrhée",
   price: 4.35,
-  category: "Douleurs",
+  category: "Digestion",
   stock: 40,
   photo: "smecta.png",
   brandphoto: "ipsen.png",
@@ -72,18 +71,88 @@ strepsils = Pill.create!(
   capacity: "24 PASTILLES"
   )
 
-strepsils = Pill.create!(
-  name: "Strepsils",
-  brand: "Reckitt Benckiser",
-  description: "Médicament pour les maux de gorge",
+cotopads = Pill.create!(
+  name: "Cotopads",
+  brand: "Lohmann",
+  description: "Ce coton est idéal pour les soins quotidien",
   price: 5.20,
-  category: "Douleurs",
+  category: "Pansements",
   stock: 80,
-  photo: "strepsils.png",
-  brandphoto: "reckitt.png",
-  capacity: "24 PASTILLES"
+  photo: "cotopads.png",
+  brandphoto: "lohmann.png",
+  capacity: "500 Pièces"
+  )
+
+exomega = Pill.create!(
+  name: "Exomega",
+  brand: "Aderma",
+  description: "Permet de traiter les sécheresses de peau",
+  price: 10.90,
+  category: "Hydratant",
+  stock: 80,
+  photo: "exomega.png",
+  brandphoto: "aderma.png",
+  capacity: "100 mL"
+  )
+
+actirub = Pill.create!(
+  name: "Actirub",
+  brand: "santeverte",
+  description: "Permet de traiter le rhume",
+  price: 11.30,
+  category: "Bien-être",
+  stock: 80,
+  photo: "actirub.png",
+  brandphoto: "santeverte.png",
+  capacity: "15 sachets"
+  )
+
+adiaril = Pill.create!(
+  name: "adiaril",
+  brand: "santeverte",
+  description: "Sachets de réhydratation pour les diarrhées du nourrisson",
+  price: 5.90,
+  category: "Bébés",
+  stock: 80,
+  photo: "adiaril.png",
+  brandphoto: "santeverte.png",
+  capacity: "125 g le sachet"
+  )
+
+ergymunil = Pill.create!(
+  name: "ergymunil",
+  brand: "nutergia",
+  description: "Complexe pour booster votre immunité",
+  price: 20.90,
+  category: "Immunité",
+  stock: 80,
+  photo: "ergymunil.png",
+  brandphoto: "nutergia.png",
+  capacity: "150 mL"
+  )
+
+ergymunil = Pill.create!(
+  name: "gallialait",
+  brand: "gallia",
+  description: "Lait infantil pour les nourrissons",
+  price: 6.30,
+  category: "Bébés",
+  stock: 80,
+  photo: "gallialait.png",
+  brandphoto: "gallia.png",
+  capacity: "500 mg"
+  )
+
+ergymunil = Pill.create!(
+  name: "suppositoires-glycerine",
+  brand: "Ipsen",
+  description: "Des suppositoires pour faire face à la constipation",
+  price: 13.40,
+  category: "Constipation",
+  stock: 80,
+  photo: "suppositoires-glycerine.png",
+  brandphoto: "ipsen.png",
+  capacity: "24 suppos"
   )
 
 puts "Pills seeded"
-
-
